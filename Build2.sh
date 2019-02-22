6e44ac79781e4d33db01931c62baa96f936fb158
gdm22 <gdm22@st-andrews.ac.uk>
2019-02-22 00:33:54 +0000

    Create Build2.sh

---
+++ Build2.sh
@@ -0,0 +1,52 @@
+cd webapps/ROOT/
+mkdir Video
+mkdir Pict
+cd Video/
+wget http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_arcd.mp4
+mv bbb_sunflower_1080p_60fps_stereo_arcd.mp4 bbb.mp4
+wget http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4
+wget http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4
+cd ../Pict/
+wget http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg
+mv BigBuckBunny.jpg bbb.jpg
+wget http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/Sintel.jpg
+wget http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/TearsOfSteel.jpg
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+~                                                                                                          
+"Build2.sh" 13L, 688C
+
+
