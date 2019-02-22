d4dc1dcd0c33a60970aaeefebd588aa2fc4508ff
gdm22 <gdm22@st-andrews.ac.uk>
2019-02-22 00:37:52 +0000

    Update Build2.sh

--- Build2.sh
+++ Build2.sh
@@ -5,7 +5,9 @@ cd Video/
 wget http://distribution.bbb3d.renderfarming.net/video/mp4/bbb_sunflower_1080p_60fps_stereo_arcd.mp4
 mv bbb_sunflower_1080p_60fps_stereo_arcd.mp4 bbb.mp4
 wget http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/Sintel.mp4
+sleep 5
 wget http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4
+sleep 5
 cd ../Pict/
 wget http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg
 mv BigBuckBunny.jpg bbb.jpg
